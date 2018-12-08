7f669e865c12ee9d84a83cebafd8dd27cddf7b47
Ines Montani <ines@ines.io>
2018-12-08 08:40:01 +0100

    Expose table and row

--- wasabi/__init__.py
+++ wasabi/__init__.py
@@ -2,5 +2,6 @@
 from __future__ import unicode_literals
 
 from .printer import Printer  # noqa
+from .tables import table, row  # noqa
 from .traceback import TracebackPrinter  # noqa
 from .util import color, wrap, get_raw_input, format_repr, MESSAGES  # noqa
