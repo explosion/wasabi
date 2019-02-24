8bac2c06bcec4d41b49aec38a14e17c126807b3a
Ines Montani <ines@ines.io>
2019-02-24 13:18:34 +0100

    Increment version

--- wasabi/about.py
+++ wasabi/about.py
@@ -1,5 +1,5 @@
 __title__ = "wasabi"
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __summary__ = "A lightweight console printing and formatting toolkit"
 __uri__ = "https://ines.io"
 __author__ = "Ines Montani"
