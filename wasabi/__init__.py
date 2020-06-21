f88ae9c4b8eb25293d68aa754371f3e8104c0b85
Ines Montani <ines@ines.io>
2020-06-21 22:19:18 +0200

    Prevent clash with built-in traceback on Python 2

--- wasabi/__init__.py
+++ wasabi/__init__.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from .printer import Printer  # noqa
 from .tables import table, row  # noqa
-from .traceback import TracebackPrinter  # noqa
+from .traceback_printer import TracebackPrinter  # noqa
 from .util import color, wrap, get_raw_input, format_repr, diff_strings  # noqa
 from .util import MESSAGES  # noqa
 
--- wasabi/tests/test_traceback.py
+++ wasabi/tests/test_traceback.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals, print_function
 
 import pytest
 import traceback
-from wasabi.traceback import TracebackPrinter
+from wasabi.traceback_printer import TracebackPrinter
 
 
 @pytest.fixture
