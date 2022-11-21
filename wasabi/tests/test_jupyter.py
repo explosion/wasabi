4afddca6d209de8c9df6ce01a373e6cdf1f0b986
Nathaniel J. Smith <njs@pobox.com>
2022-11-21 04:43:08 -0800

    Another approach to notebook testing

--- wasabi/tests/test_jupyter.py
+++ wasabi/tests/test_jupyter.py
@@ -1,19 +1,14 @@
-import pytest
-
 from pathlib import Path
 import subprocess
 import os
-import shutil
+import sys
 
 import wasabi
-
-jupyter_missing = shutil.which("jupyter") is None
 
 TEST_DATA = Path(__file__).absolute().parent / "test-data"
 WASABI_DIR = Path(wasabi.__file__).absolute().parent.parent
 
 
-@pytest.mark.skipif(jupyter_missing, reason="no jupyter install")
 def test_jupyter():
     # Make sure that the notebook can 'import wasabi', and will get the version of
     # wasabi under test. (Important if we're running tests out of the source tree.)
@@ -24,7 +19,8 @@ def test_jupyter():
         env["PYTHONPATH"] = str(WASABI_DIR)
     subprocess.run(
         [
-            "jupyter",
+            sys.executable,
+            "-m",
             "nbconvert",
             str(TEST_DATA / "wasabi-test-notebook.ipynb"),
             "--execute",
