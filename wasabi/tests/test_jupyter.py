b07624487d1b68a020d7acc3660c53e115b67aa4
Nathaniel J. Smith <njs@pobox.com>
2022-11-21 03:05:48 -0800

    Better check for jupyter install
    Avoids making mypy complain about missing jupyter stubs

--- wasabi/tests/test_jupyter.py
+++ wasabi/tests/test_jupyter.py
@@ -3,21 +3,17 @@ import pytest
 from pathlib import Path
 import subprocess
 import os
+import shutil
 
 import wasabi
 
-try:
-    import jupyter
-except ImportError:
-    have_jupyter = False
-else:
-    have_jupyter = True
+jupyter_missing = shutil.which("jupyter") is None
 
 TEST_DATA = Path(__file__).absolute().parent / "test-data"
 WASABI_DIR = Path(wasabi.__file__).absolute().parent.parent
 
 
-@pytest.mark.skipif(not have_jupyter, reason="no jupyter install")
+@pytest.mark.skipif(jupyter_missing, reason="no jupyter install")
 def test_jupyter():
     # Make sure that the notebook can 'import wasabi', and will get the version of
     # wasabi under test. (Important if we're running tests out of the source tree.)
