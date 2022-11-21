8d5ae10320350ea6417eb319477957b5757c5e69
Nathaniel J. Smith <njs@pobox.com>
2022-11-21 02:37:35 -0800

    Fix jupyter test

--- wasabi/tests/test_jupyter.py
+++ wasabi/tests/test_jupyter.py
@@ -1,11 +1,22 @@
 from pathlib import Path
 import subprocess
+import os
+
+import wasabi
 
 TEST_DATA = Path(__file__).absolute().parent / "test-data"
-
+WASABI_DIR = Path(wasabi.__file__).absolute().parent.parent
 
 def test_jupyter():
-    result = subprocess.run(
+    # Make sure that the notebook can 'import wasabi', and will get the version of
+    # wasabi under test. (Important if we're running tests out of the source tree.)
+    env = dict(os.environ)
+    if "PYTHONPATH" in env:
+        env["PYTHONPATH"] = f"{WASABI_DIR}{os.pathsep}{env['PYTHONPATH']}"
+    else:
+        env["PYTHONPATH"] = str(WASABI_DIR)
+    print(env["PYTHONPATH"])
+    subprocess.run(
         [
             "jupyter",
             "nbconvert",
@@ -15,5 +26,6 @@ def test_jupyter():
             "--to",
             "notebook",
         ],
+        env=env,
         check=True,
     )
